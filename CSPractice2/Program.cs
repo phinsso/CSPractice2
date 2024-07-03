ae0b44d8d0dc0d8a6e42787e8d6f9b1bab61f570
phinsso <naso01247@gmail.com>
2024-07-03 09:18:58 +0900

    #6 23-7. 하이딩

--- CSPractice2/Program.cs
+++ CSPractice2/Program.cs
@@ -8,12 +8,44 @@ namespace CSPractice2
 {
     internal class Program
     {
+
+        class Parent
+        {
+            public int variable = 273;
+
+            public void Method()
+            {
+                Console.WriteLine("부모 메서드");
+            }
+        }
+
+        class Child : Parent
+        {
+            public string variable = "하이딩";
+
+            public void Method()
+            {
+                Console.WriteLine("자식 메서드");
+            }
+        }
+
         static void Main(string[] args)
         {
 
             // #6 23-7. 섀도잉
             int number = 20;
             Console.WriteLine(number); // 20
+
+            // #6 23-7. 하이딩
+            Child child = new Child();
+            Console.WriteLine(child.variable); // 하이딩
+            child.Method(); // 자식 메서드
+            Console.WriteLine(((Parent)child).variable); // 273
+            ((Parent)child).Method(); // 부모 메서드
+            Parent p = child;
+            Console.WriteLine(p.variable); // 273
+            p.Method(); // 부모 메서드
+            ((Child)p).Method(); // 자식 메서드
         }
     }
 }
